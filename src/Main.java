2470e0e1579a7fd975192a6c0c3660da19f76863
Zane41 <korener@rambler.ru>
2024-04-11 13:24:49 +0300

    перенес рандомайзер в функцию

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,14 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-      int min =1; int max =50;
-      int b = (int) (Math.random() * (max - min)) +min;
 
       Tree testTree = new Tree();
       Squirell testSquirell = new Squirell();
-      double x= testSquirell.collectNuts(testTree.createArray(b));
-
+      double x= testSquirell.collectNuts(testTree.createArray(randomizer()));
       testSquirell.say(x);
+    }
+    public static int randomizer (){
+        int min =1; int max =50;
+        int b = (int) (Math.random() * (max - min)) +min; return b;
     }
 }
